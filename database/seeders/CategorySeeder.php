bab304306330a829193f474a708221c28a3befcc
Victor Purice <victor.purice@ulbsibiu.ro>
2025-04-06 02:50:07 +0300

    feat: added seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            'category_name' => Str::random(5),
+        ]);
+    }
+}
---
+++ database/seeders/SkillSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class SkillSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('skills')->insert([
+            'skill_name' => Str::random(5),
+        ]);
+    }
+}
